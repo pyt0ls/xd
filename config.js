43eed196fc2b1744f69cc0fd341ebda9e7e98255
pytols <130239705+pyt0ls@users.noreply.github.com>
2025-12-31 04:39:39 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+export const config = {
+  BASE_API: 'http://149.56.18.68:25605',
+  API_KEY: 'dexmito2025'
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import { consultarDark } from './services/darkApi.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/:tipo/:dado', async (req, res) => {
+  const { tipo, dado } = req.params;
+
+  try {
+    const resultado = await consultarDark(tipo, dado);
+    res.json({ ok: true, resultado });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      ok: false,
+      error: 'Erro ao consultar API'
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+  console.log(`🔥 Backend rodando na porta ${PORT}`);
+});
