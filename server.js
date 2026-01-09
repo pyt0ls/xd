93347387b44efede3ea2ce06d56b80c7467ea63b
pytols <130239705+pyt0ls@users.noreply.github.com>
2026-01-09 06:50:38 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import { consultarDark } from './services/DarkApi.js';
+import authRoutes from './routes/auth.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', authRoutes);
 
 app.get('/api/:tipo/:dado', async (req, res) => {
   const { tipo, dado } = req.params;
